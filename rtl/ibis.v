46c9f42a1101dbcde321c6fab8e651ea9b5e4410
Roland Metivier <metivier.roland@chlorophyt.us>
2024-06-13 17:29:24 -0500

    actually change tabs to spaces this time

--- rtl/ibis.v
+++ rtl/ibis.v
@@ -4,47 +4,47 @@ module ibis
  (input wire aclk,
   input wire aresetn,
   input wire enable,
-	// ==========================================================================
-	// AXI4 write address bus
-	// TODO: awaddr
-	input wire awvalid,
-	output wire awready,
-	// AXI4 write data bus
-	input wire [31:0] wdata,
-	input wire wvalid,
-	output wire wready,
-	// AXI4 write response bus
-	output wire [1:0] bresp,
-	output wire bvalid,
-	input wire bready,
-	// ==========================================================================
-	// AXI4 read address bus
-	// TODO: araddr
-	input wire arvalid,
-	output wire arready,
-	// AXI4 read data bus
-	output wire [31:0] rdata,
-	output wire [1:0] rresp,
-	output wire rvalid,
-	input wire rready);
+  // ==========================================================================
+  // AXI4 write address bus
+  // TODO: awaddr
+  input wire awvalid,
+  output wire awready,
+  // AXI4 write data bus
+  input wire [31:0] wdata,
+  input wire wvalid,
+  output wire wready,
+  // AXI4 write response bus
+  output wire [1:0] bresp,
+  output wire bvalid,
+  input wire bready,
+  // ==========================================================================
+  // AXI4 read address bus
+  // TODO: araddr
+  input wire arvalid,
+  output wire arready,
+  // AXI4 read data bus
+  output wire [31:0] rdata,
+  output wire [1:0] rresp,
+  output wire rvalid,
+  input wire rready);
 
-	ibis_axi4 axi4(
-		.aclk(aclk),
-		.aresetn(aresetn),
-		.enable(enable),
-		.awvalid(awvalid),
-		.awready(awready),
-		.wdata(wdata),
-		.wvalid(wvalid),
-		.wready(wready),
-		.bresp(bresp),
-		.bvalid(bvalid),
-		.bready(bready),
-		.arvalid(arvalid),
-		.arready(arready),
-		.rdata(rdata),
-		.rresp(rresp),
-		.rvalid(rvalid),
-		.rready(rready)
-	);
+  ibis_axi4 axi4(
+    .aclk(aclk),
+    .aresetn(aresetn),
+    .enable(enable),
+    .awvalid(awvalid),
+    .awready(awready),
+    .wdata(wdata),
+    .wvalid(wvalid),
+    .wready(wready),
+    .bresp(bresp),
+    .bvalid(bvalid),
+    .bready(bready),
+    .arvalid(arvalid),
+    .arready(arready),
+    .rdata(rdata),
+    .rresp(rresp),
+    .rvalid(rvalid),
+    .rready(rready)
+  );
 endmodule: ibis
--- rtl/ibis_axi4.sv
+++ rtl/ibis_axi4.sv
@@ -5,26 +5,26 @@ module ibis_axi4
   input wire logic aresetn,
   input wire logic enable,
 
-	// AXI4 write address bus
-	// TODO: awaddr
-	input wire logic awvalid,
-	output logic awready,
-	// AXI4 write data bus
-	input wire logic unsigned [31:0] wdata,
-	input wire logic wvalid,
-	output logic wready,
-	// AXI4 write response bus
-	output logic [1:0] bresp,
-	output logic bvalid,
-	input wire logic bready,
+  // AXI4 write address bus
+  // TODO: awaddr
+  input wire logic awvalid,
+  output logic awready,
+  // AXI4 write data bus
+  input wire logic unsigned [31:0] wdata,
+  input wire logic wvalid,
+  output logic wready,
+  // AXI4 write response bus
+  output logic [1:0] bresp,
+  output logic bvalid,
+  input wire logic bready,
 
-	// AXI4 read address bus
-	// TODO: araddr
-	input wire logic arvalid,
-	output logic arready,
-	// AXI4 read data bus
-	output logic unsigned [31:0] rdata,
-	output logic [1:0] rresp,
-	output logic rvalid,
-	input wire logic rready);
+  // AXI4 read address bus
+  // TODO: araddr
+  input wire logic arvalid,
+  output logic arready,
+  // AXI4 read data bus
+  output logic unsigned [31:0] rdata,
+  output logic [1:0] rresp,
+  output logic rvalid,
+  input wire logic rready);
 endmodule: ibis_axi4
